74283063dcd0b4262376a088ccfccd00506c79d1
RoronuaHong <807672412@qq.com>
2024-09-13 07:07:56 +0800

    完成createElement

---
+++ src/MyReact/index.jsx
@@ -0,0 +1,5 @@
+// https://pomb.us/build-your-own-react/
+
+import createElement from './createElement'
+
+export { createElement }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,16 @@
 import React from 'react'
+import { createElement } from '../MyReact'
+
+const element = createElement(
+  'h1', 
+  { id: 'title', class: 'hello' }, 
+  'HelloWorld',
+  createElement(
+    'h2'
+  )
+)
+
+console.log(element)
 
 const Home: React.FC = () => {
   return (
